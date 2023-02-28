6a1efb9cb725270ce6a5375c174a0e60bf6d8b14
Hanzo48 <123712541+Hanzo48@users.noreply.github.com>
2023-02-28 02:24:04 -0300

    incrustar texto al HTML

---
+++ practicas/javascript/03/app.js
@@ -0,0 +1,34 @@
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+const submit = document.getElementById('submit')
+const incrustar = document.getElementById('incrustar')
+
+
+Itext = ""
+
+ form[1].addEventListener('click',(e) => {
+        // e.preventDefault()    
+        const Itext = form[0].value
+        form[0].value = ''
+        // form.classList.add('bckcolor')
+        const ItemList = document.createElement('li')
+        ItemList.textContent = Itext
+        incrustar.children[0].appendChild(ItemList)
+})
+
+console.log(Itext)
+// input.addEventListener('keyup',(e)=>{
+//     console.log(e)
+    
+// })
+
+console.log(incrustar.children[0])
+
+
+
+
+// input.addEventListener('keyup', (e) => {
+    //     console.log(e.key)    
+    // })
+    
+    
